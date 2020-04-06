b61a646c84044bfc58fef5de0ba59dba7f177270
dav3rid <davecodingstuff@gmail.com>
2020-04-06 14:28:26 +0100

    added home

--- src/App.js
+++ src/App.js
@@ -1,31 +1,21 @@
 import React from 'react';
-import socketIOClient from 'socket.io-client';
-import { Router } from '@reach/router';
+import { Router, Link } from '@reach/router';
 import './App.css';
 import Choose from './components/Choose';
 import Host from './components/Host';
 import Join from './components/Join';
 
-class App extends React.Component {
-  // state = {
-  //   isPlayerOne: null,
-  //   playerOneName: '',
-  //   playerTwoName: '',
-  // };
-
-  componentDidMount() {}
-
-  render() {
-    return (
-      <div className="App">
-        <Router>
-          <Choose path="/" />
-          <Host path="/host" />
-          <Join path="/join" />
-        </Router>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <Link to="/">HOME</Link>
+      <Router>
+        <Choose path="/" />
+        <Host path="/host" />
+        <Join path="/join" />
+      </Router>
+    </div>
+  );
+};
 
 export default App;
