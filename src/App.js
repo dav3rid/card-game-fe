73045ce2b7f78240242ac8a85a6563594338e5d3
dav3rid <davecodingstuff@gmail.com>
2020-04-06 14:11:26 +0100

    added Host and Join components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import socketIOClient from 'socket.io-client';
+import { Router, Link } from '@reach/router';
 import './App.css';
+import Host from './components/Host';
+import Join from './components/Join';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  // state = {
+  //   isPlayerOne: null,
+  //   playerOneName: '',
+  //   playerTwoName: '',
+  // };
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Host path="/host" />
+          <Join path="/join" />
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Host.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Host extends Component {
+  render() {
+    return <div>HOST</div>;
+  }
+}
+
+export default Host;
